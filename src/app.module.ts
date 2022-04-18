f749adef01b7c9322fcc42b197c4ba56cc94948c
nursultan <prouser.01@mail.ru>
2022-04-18 18:16:59 +0600

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BlogModule } from './blog/blog.module';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    BlogModule,
+    AuthModule,
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
